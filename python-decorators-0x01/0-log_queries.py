60c444681ead47b038dc698d7a3620f505c7f0de
Olumide <adeoyeolumbah@gmail.com>
2025-09-09 10:31:55 +0100

    Added datetime into the decorator

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,17 +1,22 @@
 import sqlite3
 import functools
+from datetime import datetime
 
-# Decorator to log SQL queries
+
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         query = kwargs.get("query") or (args[0] if args else None)
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
         if query:
-            print(f"[LOG] Executing SQL query: {query}")
+            print(f"[{timestamp}] Executing SQL query: {query}")
         else:
-            print("[LOG] No SQL query found to log.")
+            print(f"[{timestamp}] No SQL query found to log.")
+
         return func(*args, **kwargs)
     return wrapper
+
 
 @log_queries
 def fetch_all_users(query):
