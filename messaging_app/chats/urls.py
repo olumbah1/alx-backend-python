c7504aacdcf8b00faa4868fb497f0778dd094633
Olumide <adeoyeolumbah@gmail.com>
2025-09-16 21:38:19 +0100

    Added a nested router that create a url that represent a parent-child relationship between resources

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,18 @@
-from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework_nested import routers
 from .views import CustomUserViewSet, ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
-router.register(r'customuser', CustomUserViewSet, basename='customuser')
-router.register(r'conversation', ConversationViewSet, basename='conversation')
-router.register(r'message', MessageViewSet, basename='message')
+router = routers.DefaultRouter()
+router.register(r'user', CustomUserViewSet)
+router.register(r'conversations', ConversationViewSet)
+router.register(r'messages', MessageViewSet)
 
-urlpatterns = router.urls
+conversations_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include(conversations_router.urls)),
+]
+
