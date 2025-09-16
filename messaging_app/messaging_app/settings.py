b23e1a417aa28033deb8dde5ee5c411748c26b3e
Olumide <adeoyeolumbah@gmail.com>
2025-09-16 12:13:25 +0100

    Created models for chats app

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
+AUTH_USER_MODEL = 'chats.CustomUser'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
